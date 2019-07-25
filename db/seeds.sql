3866c16486a8ee3c84f5bb2b93f1341e34ac2e04
jimmyrdz <ardzjaime@gmail.com>
2019-07-25 02:01:31 -0500

    added db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Double Bacon Deluxe', FALSE);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Plain n Dry', FALSE);
